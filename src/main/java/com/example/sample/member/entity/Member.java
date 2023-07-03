e50d2f7a5c36cd91b8cd068f7a90101ae66f45af
ksj2083 <ksj2083@naver.com>
2023-07-03 16:37:56 +0900

    feat : postgreSQL 연동 및 member 테이블 생성 이 코드는 샘플코드로, JPA를 사용하여 테이블을 만드는 프로그램임

---
+++ src/main/java/com/example/sample/member/entity/Member.java
@@ -0,0 +1,23 @@
+package com.example.sample.member.entity;
+
+import lombok.Getter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userName; // 로그인용 아이디
+
+    private String password;
+
+    private String nickName;
+
+}
